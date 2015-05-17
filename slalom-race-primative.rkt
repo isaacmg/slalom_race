5e5e4c2604beef7077a02ba04c21e78736bbec29
Isaac Godfried <igodfried@isaac26.com>
2015-05-17 16:45:55 -0400

    Updates

--- slalom-race-primative.rkt
+++ slalom-race-primative.rkt
@@ -7,10 +7,27 @@
       gates
       (slalom-race-loop (-  gates 1) class name)))
 
+;;Boats 
 (define (make-boat some-boat) 
   some-boat
  ) 
-(define (r list2) 
+(
+;;List of gates
+(define (gate-list list2) 
   list2)
+(define (distance x y z) 
+  (list x y z))
+ 
+(define (upstream gate boat)
+  (list gate distance eddy-length) 
+  )
+(define (reverse gate)
+  (list gate 
+  ))
+(define (drop x y z) 
+  z) 
+;;Moves 
+
+ 
 
 
